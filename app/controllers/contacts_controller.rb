3cc37628362811d01ab884cf19c0f37eeada206c
fred oliveira <fredoliveira.sys@gmail.com>
2018-10-10 00:26:23 +0000

    Refatora caixa de seleção v2.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,10 +15,12 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    options_for_select
   end
 
   # GET /contacts/1/edit
   def edit
+    options_for_select
   end
 
   # POST /contacts
@@ -62,6 +64,11 @@ class ContactsController < ApplicationController
   end
 
   private
+
+    def options_for_select
+      @kind_options_for_select = Kind.all
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
       @contact = Contact.find(params[:id])
